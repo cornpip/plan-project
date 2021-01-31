ed45a349eeb90a092be215e077f4d669e48c5c3a
cornpip <74674780+cornpip@users.noreply.github.com>
2021-02-01 05:27:08 +0900

    0201

--- clock.js
+++ clock.js
@@ -1,4 +1,17 @@
-let day = new Date();
+let today = new Date();
+console.log(today);
 
-console.log(day.getDay());
-console.log("hi");
+console.log(today.getDate());
+console.log(today.getMonth()+1);
+console.log(today.getFullYear());
+
+let week = new Array('SUN','MON','SAT','WED','THU','FRI','SAT');
+
+console.log(week[today.getDay()]);
+console.log(today.getHours());
+console.log(today.getMinutes());
+console.log(today.getSeconds());
+
+console.log(today.toLocaleDateString());
+console.log(today.toLocaleTimeString());
+console.log(today.toLocaleString());
